3c84ccc8bc8f5e299735ebb17d7a7f344310df89
Om <omtakalkar9@gmail.com>
2016-10-21 23:13:18 +0530

    second commit

--- src/newsarstechnica.java
+++ src/newsarstechnica.java
@@ -2,6 +2,9 @@
 	import java.io.IOException;
 	import java.io.InputStreamReader;
 	import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 	public class newsarstechnica 
 	{
 	  public static void main(String [] args)
@@ -17,11 +20,15 @@
 	       
 	    BufferedReader in = new BufferedReader (new InputStreamReader(rssURL.openStream()));
 	    
+	    
 	    String line;
 		String  sourceCode = "";
-		while ((line = in.readLine()) != null) {
+		
+		while ((line = in.readLine()) != null) 
+		{
 		    int titleEndIndex = 0;
 		    int titleStartIndex = 0;
+		   
 		    while (titleStartIndex >= 0)
 		    {
 		        titleStartIndex = line.indexOf("title", titleEndIndex);
@@ -29,11 +36,31 @@
 		        {
 		            titleEndIndex = line.indexOf("," ,titleStartIndex);
 		            sourceCode += line.substring(titleStartIndex + "title".length(), titleEndIndex) + "\n";
+		            
 		        }
+		        
 		    }
+		   
+		    
 		}
-	 in.close(); 
+		String[] splitted = sourceCode.split(" ");
+        Map<String, Integer> hm = new HashMap<String, Integer>();
+        for (int i=0; i<splitted.length ; i++)
+        {
+            if (hm.containsKey(splitted[i])) 
+            {
+               int cont = hm.get(splitted[i]);
+               hm.put(splitted[i], cont + 1);
+            } else 
+            {
+               hm.put(splitted[i], 1);
+            }
+         }
+         System.out.println(hm+"\n");
+		
+		in.close(); 
 	return sourceCode;
+	
 	}
 	catch(IOException ioe)
 	{
